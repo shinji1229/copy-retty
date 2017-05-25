c45b6b180bb23bc349797ad0bd69ed63b8da363d
shinji1229 <fk.shinji1229@gmail.com>
2017-05-25 20:44:16 +0900

    変更を記述

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<Elastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '13.58.50.215', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
