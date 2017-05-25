c0f568a9c6a7d3c669b66a86012becc5bb0ad0da
shinji1229 <fk.shinji1229@gmail.com>
2017-05-25 19:01:44 +0900

    デプロイ環境別の設定を記述

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '<Elastic IP>', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
