5e3445a200ce8fb2607a8762b7d2d00d0e593518
shinji1229 <fk.shinji1229@gmail.com>
2017-06-04 21:50:16 +0900

    アソシエーションを定義

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+   has_many :users, through: :restaurant_users
+   has_many :restaurant_users
+   has_many :comments
 end
--- app/models/restaurant_user.rb
+++ app/models/restaurant_user.rb
@@ -1,2 +1,4 @@
 class RestaurantUser < ApplicationRecord
+  belongs_to :user
+  belongs_to :restaurant
 end
