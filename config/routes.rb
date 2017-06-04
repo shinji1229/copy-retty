8a473800ab5d24f9fb0c02bfe6667058d2d27a57
shinji1229 <fk.shinji1229@gmail.com>
2017-06-04 21:42:05 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
-  resources :users do
+  resources :restaurants do
     resources :comments
   end
-  resources :restaurants
 end
