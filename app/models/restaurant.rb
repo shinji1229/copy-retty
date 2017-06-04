de06b02a76fe649817a5f3a372b64446f3706477
shinji1229 <fk.shinji1229@gmail.com>
2017-06-04 22:01:39 +0900

    restaurant_modelにDeviseを実装

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
    has_many :users, through: :restaurant_users
    has_many :restaurant_users
    has_many :comments
