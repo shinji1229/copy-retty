cc0cdbb8e7cf653909821997978db104f4edc2c0
shinji1229 <fk.shinji1229@gmail.com>
2017-06-06 18:56:13 +0900

    ファイルをmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604133621) do
+ActiveRecord::Schema.define(version: 20170606055827) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",          limit: 65535
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 20170604133621) do
   end
 
   create_table "restaurants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
-    t.integer  "phonenumber",                         null: false
-    t.integer  "budget",                              null: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -48,8 +43,9 @@ ActiveRecord::Schema.define(version: 20170604133621) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_restaurants_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_restaurants_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true, using: :btree
   end
 
