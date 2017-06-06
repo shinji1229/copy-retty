7724fc25b6455d14172d30a86ecdffab6a5f2a79
shinji1229 <fk.shinji1229@gmail.com>
2017-06-06 19:53:29 +0900

    Nameカラムをdeviseで使用可能にする

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :restaurants, controllers: {
-    sessions:      'restaurants/sessions',
-    passwords:     'restaurants/passwords',
-    registrations: 'restaurants/registrations'
-  }
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
+  devise_for :restaurants
+  # devise_for :restaurants, controllers: {
+  #   sessions:      'restaurants/sessions',
+  #   passwords:     'restaurants/passwords',
+  #   registrations: 'restaurants/registrations'
+  # }
+  # devise_for :users, controllers: {
+  #   sessions:      'users/sessions',
+  #   passwords:     'users/passwords',
+  #   registrations: 'users/registrations'
+  # }
   root 'users#show'
   resources :users, only: [:index, :show]
   resources :restaurants do
