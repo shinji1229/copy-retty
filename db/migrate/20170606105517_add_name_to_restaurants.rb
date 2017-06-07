8635ccd1fc3bbe579034ac606499dc53bec51869
shinji1229 <fk.shinji1229@gmail.com>
2017-06-07 14:01:15 +0900

    ミスを修正

--- db/migrate/20170606105517_add_name_to_restaurants.rb
+++ db/migrate/20170606105517_add_name_to_restaurants.rb
@@ -1,7 +1,7 @@
 class AddNameToRestaurants < ActiveRecord::Migration[5.0]
   def change
     add_column :restaurants, :name, :string, index: true, null: false, unique: true
-    add_column :restaurants, :phonenumber, :string, null: false, unique: true
-    add_column :restaurants, :budget, :integer,null: false
+    add_column :restaurants, :phonenumber, :string, unique: true
+    add_column :restaurants, :budget, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 20170606105517) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name",                                null: false
-    t.string   "phonenumber",                         null: false
-    t.integer  "budget",                              null: false
+    t.string   "phonenumber"
+    t.integer  "budget"
     t.index ["email"], name: "index_restaurants_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true, using: :btree
   end
