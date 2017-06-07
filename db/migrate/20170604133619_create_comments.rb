eaa16315928fd7b75ba5ce6ecfe765e7f54951ca
shinji1229 <fk.shinji1229@gmail.com>
2017-06-07 14:06:54 +0900

    Rateカラムを追加

--- db/migrate/20170604133619_create_comments.rb
+++ db/migrate/20170604133619_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text         :body
+      t.text         :body, null: false
       t.string       :image
+      t.integer      :rate, null: false
       t.references   :user ,foreign_key: true
       t.references   :restaurant ,foreign_key: true
       t.timestamps
