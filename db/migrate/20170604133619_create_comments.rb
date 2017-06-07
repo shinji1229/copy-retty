3b95c151e51ee5ae129efaa91194cf1e253b9975
shinji1229 <fk.shinji1229@gmail.com>
2017-06-07 14:50:15 +0900

    commentsテーブルにrateカラムを追加

--- db/migrate/20170604133619_create_comments.rb
+++ db/migrate/20170604133619_create_comments.rb
@@ -1,9 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text         :body, null: false
+      t.text         :body
       t.string       :image
-      t.integer      :rate, null: false
       t.references   :user ,foreign_key: true
       t.references   :restaurant ,foreign_key: true
       t.timestamps
