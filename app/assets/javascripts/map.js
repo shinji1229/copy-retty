f666681581bc270e3f03ec381aaf3b516c93f6e6
shinji1229 <fk.shinji1229@gmail.com>
2017-06-10 16:53:41 +0900

    Googlemap APIを導入

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,29 @@
+var googleMap = {
+  init: function() {
+    var self = this;
+    var geocoder = new google.maps.Geocoder();
+    var address = '東京都世田谷区';
+    self.geocode(geocoder, address);
+  },
+  geocode: function(geocoder, address) {
+    var self = this;
+    geocoder.geocode({
+      'address': address
+    }, function(result, status) {
+      self.geocodeCallback(result, status)
+    });
+  },
+  geocodeCallback: function(result, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      var latlng = result[0].geometry.location;
+      var options = {
+        zoom: 15,
+        center: latlng,
+        mapTypeId: 'roadmap'
+      };
+      var map = new google.maps.Map(document.getElementById('map'), options);
+    } else {
+      alert('エラーです！');
+    }
+  }
+}
