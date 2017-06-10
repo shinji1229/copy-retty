62127e3908f984e153c9115d18e6a4094d42c92a
shinji1229 <fk.shinji1229@gmail.com>
2017-06-10 20:44:15 +0900

    Addressデータをjsで取得できるようにする

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,7 +2,7 @@ var googleMap = {
   init: function() {
     var self = this;
     var geocoder = new google.maps.Geocoder();
-    var address = '東京都目黒区下目黒';
+    var address = $('.restaurant_address').val();;
     self.geocode(geocoder, address);
   },
   geocode: function(geocoder, address) {
