cee1116b5a651ad31ecf9fb575840e8798f6c10c
shinji1229 <fk.shinji1229@gmail.com>
2017-06-10 16:04:15 +0900

    ログイン条件を変更

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 
-  before_action :authenticate_restaurant!
+  before_action :authenticate_restaurant!, except: [:search,:show]
 
   def index
   end
