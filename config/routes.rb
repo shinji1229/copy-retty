e200212333e777ba24d3198e044495a51fb05824
shinji1229 <fk.shinji1229@gmail.com>
2017-06-10 14:12:14 +0900

    不要な記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :restaurants
-  # devise_for :restaurants, controllers: {
-  #   sessions:      'restaurants/sessions',
-  #   passwords:     'restaurants/passwords',
-  #   registrations: 'restaurants/registrations'
-  # }
-  # devise_for :users, controllers: {
-  #   sessions:      'users/sessions',
-  #   passwords:     'users/passwords',
-  #   registrations: 'users/registrations'
-  # }
   root 'users#show'
   resources :users, only: [:index, :show] do
     collection do
