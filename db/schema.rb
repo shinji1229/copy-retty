f67950b69b6b9b696c94fb3e05e445b57de309e1
shinji1229 <fk.shinji1229@gmail.com>
2017-06-11 15:37:50 +0900

    Imageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609052441) do
+ActiveRecord::Schema.define(version: 20170611063704) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",          limit: 65535
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170609052441) do
     t.integer  "budget_dinner"
     t.string   "country"
     t.string   "address"
+    t.string   "image"
     t.index ["email"], name: "index_restaurants_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true, using: :btree
   end
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(version: 20170609052441) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
