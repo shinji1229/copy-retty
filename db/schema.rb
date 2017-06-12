ff7a12ac894f16a1f889273658c9ec784fd09536
shinji1229 <fk.shinji1229@gmail.com>
2017-06-12 18:19:54 +0900

    変更を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611074413) do
+ActiveRecord::Schema.define(version: 20170612091548) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",          limit: 65535
@@ -44,24 +44,25 @@ ActiveRecord::Schema.define(version: 20170611074413) do
   end
 
   create_table "restaurants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name",                                null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "name",                                              null: false
     t.string   "phonenumber"
     t.integer  "budget_lunch"
     t.integer  "budget_dinner"
     t.string   "country"
     t.string   "address"
+    t.text     "kana",                   limit: 65535
     t.index ["email"], name: "index_restaurants_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true, using: :btree
   end
