a36e1f5b2217b4e50b333076d0a2abed20f7a517
shinji1229 <fk.shinji1229@gmail.com>
2017-06-14 16:26:31 +0900

    画像を投稿できるようにする

--- app/uploaders/comments_image_uploader.rb
+++ app/uploaders/comments_image_uploader.rb
@@ -7,7 +7,7 @@ class CommentsImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-
+  process resize_to_fit: [150, 150]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
