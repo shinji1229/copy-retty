493ff2ad8aea4c4cc441b84f9f548eb32f3616cd
shinji1229 <fk.shinji1229@gmail.com>
2017-06-14 17:15:07 +0900

    carrierwave.rbを作成

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
+  }
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = ENV['AWS_BUCKET_NAME']
+        config.asset_host = ENV['AWS_BUCKET_URL']
+    when 'production'
+        config.fog_directory  = ENV['AWS_BUCKET_NAME']
+        config.asset_host = ENV['AWS_BUCKET_URL']
+    end
+end
