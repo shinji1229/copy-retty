ee9cba9234ac52a6371c12208eb721a618601e53
shinji1229 <fk.shinji1229@gmail.com>
2017-06-14 17:10:50 +0900

    保存先をfogに変更

--- app/uploaders/comments_image_uploader.rb
+++ app/uploaders/comments_image_uploader.rb
@@ -5,8 +5,8 @@ class CommentsImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+
   storage :fog
-  # storage :fog
   process resize_to_fit: [150, 150]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
