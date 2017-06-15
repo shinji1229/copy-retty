5a2e60cad1d097227d0d091290ca06d8aa739a42
shinji1229 <fk.shinji1229@gmail.com>
2017-06-15 13:56:59 +0900

    secrets.ymlにaccess_keyを記述

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,17 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key_id,
+    region: Rails.application.secrets.aws_region_id
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = ENV['AWS_BUCKET_NAME']
-        config.asset_host = ENV['AWS_BUCKET_URL']
+        config.fog_directory  = Rails.application.secrets.aws_bucket_name_id
+        config.asset_host = Rails.application.secrets.aws_bucket_url_id
     when 'production'
-        config.fog_directory  = ENV['AWS_BUCKET_NAME']
-        config.asset_host = ENV['AWS_BUCKET_URL']
+        config.fog_directory  = Rails.application.secrets.aws_bucket_name_id
+        config.asset_host = Rails.application.secrets.aws_bucket_url_id
     end
 end
