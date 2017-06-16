72ef134a8c8092d41508387b187793966ed39d6d
shinji1229 <fk.shinji1229@gmail.com>
2017-06-16 20:54:55 +0900

    カラム名を修正

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :phone, presence: true, format: {with: /\A[0-9-]{,14}\z/}
+  validates :phonenumber, presence: true, format: {with: /\A[0-9-]{,14}\z/}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :users, through: :restaurant_users
