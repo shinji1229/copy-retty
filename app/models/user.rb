fe4422dcb9df56b47c713deaaff63310811365d3
shinji1229 <fk.shinji1229@gmail.com>
2017-06-17 19:16:25 +0900

    アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_many :restaurants, through: :restautant_users
   has_many :restaurant_users
   has_many :comments
+  has_many :events
   mount_uploader :image, UserImageUploader
 end
