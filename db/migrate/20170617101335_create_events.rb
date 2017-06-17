91d62ff5cad7a770f32b57b5018dd026561a2281
shinji1229 <fk.shinji1229@gmail.com>
2017-06-17 19:20:46 +0900

    カラムを設定

--- db/migrate/20170617101335_create_events.rb
+++ db/migrate/20170617101335_create_events.rb
@@ -1,6 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
+      t.string     :title
+      t.datetime   :start
+      t.datetime   :end
+      t.references :user ,foreign_key: true
 
       t.timestamps
     end
