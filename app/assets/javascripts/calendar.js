4ad579ce8afc999f744651a4c7a463531b86befb
shinji1229 <fk.shinji1229@gmail.com>
2017-06-19 22:12:25 +0900

    カレンダーのviewを編集

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-      contentHeight: 500,
-      lang: 'ja',
-      navLinks: true,
-      selectable: true,
-      selectHelper: true,
-      select: function(start, end) {
-        var title = prompt('イベントを追加');
-        var eventData;
-        if (title) {
-          eventData = {
-            title: title,
-            start: start,
-            end: end
-          };
-          $('#calendar').fullCalendar('renderEvent', eventData, true);
-        }
-        $('#calendar').fullCalendar('unselect');
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
       },
-      events: '/events.json',
-      editable: true
+      contentHeight: 500,
+      lang: 'ja'
     });
 });
