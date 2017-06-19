37bc9104071e859db56e18a83a81f65dfb43f3de
shinji1229 <fk.shinji1229@gmail.com>
2017-06-19 12:15:40 +0900

    バリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :start, presence: true
+  validates :end, presence: true
   belongs_to :users
 end
