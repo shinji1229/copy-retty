4e8744554a2184e17b2feffce66d7cb0708eac6c
shinji1229 <fk.shinji1229@gmail.com>
2017-06-19 12:10:57 +0900

    ネストを記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :restaurants
   root 'entrances#index'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :events, only: [:new, :create, :destroy]
+  end
   resources :restaurants, only: [:index, :show] do
     collection do
       get 'search'
