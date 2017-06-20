37b2a2ff461d72af1516254a5eb39a6dbc9e8e3c
shinji1229 <fk.shinji1229@gmail.com>
2017-06-20 13:55:32 +0900

    カレンダーを表示できるようにする

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      contentHeight: 500,
-      lang: 'ja',
-      navLinks: true,
-      selectable: true,
-      selectHelper: true,
-      select: function(start, end) {
-        var title = prompt('イベントを追加');
-        var eventData;
-        if (title) {
-          eventData = {
-            title: title,
-            start: start,
-            end: end
-          };
-          $('#calendar').fullCalendar('renderEvent', eventData, true);
-        }
-        $('#calendar').fullCalendar('unselect');
-      },
-      events: '/events.json',
-      editable: true
-    });
+$(document).on('turbolinks:load', function () {
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    contentHeight: 500,
+    lang: 'ja',
+    navLinks: true,
+    selectable: true,
+    selectHelper: true,
+    select: function(start, end) {
+      var title = prompt('イベントを追加');
+      var eventData;
+      if (title) {
+        eventData = {
+          title: title,
+          start: start,
+          end: end
+        };
+        $('#calendar').fullCalendar('renderEvent', eventData, true);
+      }
+      $('#calendar').fullCalendar('unselect');
+    },
+    events: '/events.json',
+    editable: true
+  });
 });
