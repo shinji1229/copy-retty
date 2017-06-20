4fce3c8aa7f5848d17baafcc882399535e83d2d4
shinji1229 <fk.shinji1229@gmail.com>
2017-06-20 14:43:34 +0900

    リダイレクト先でもgooglemapを表示させる

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,38 +1,37 @@
-if(window.location.href.match(/\/restaurants\/\d+/))
-{
-  var googleMap = {
-    init: function() {
-      var self = this;
-      var geocoder = new google.maps.Geocoder();
-      var address = $('.restaurant_address').val();;
-      self.geocode(geocoder, address);
-    },
-    geocode: function(geocoder, address) {
-      var self = this;
-      geocoder.geocode({
-        'address': address
-      }, function(result, status) {
-        self.geocodeCallback(result, status)
-      });
-    },
-    geocodeCallback: function(result, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        var latlng = result[0].geometry.location;
-        var options = {
-          zoom: 17,
-          center: latlng,
-          mapTypeId: 'roadmap'
-        };
-        var map = new google.maps.Map(document.getElementById('map'), options);
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-          });
+  if(window.location.href.match(/\/restaurants\/\d+/))
+  {
+    var googleMap = {
+      init: function() {
+        var self = this;
+        var geocoder = new google.maps.Geocoder();
+        var address = $('.restaurant_address').val();;
+        self.geocode(geocoder, address);
+      },
+      geocode: function(geocoder, address) {
+        var self = this;
+        geocoder.geocode({
+          'address': address
+        }, function(result, status) {
+          self.geocodeCallback(result, status)
+        });
+      },
+      geocodeCallback: function(result, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          var latlng = result[0].geometry.location;
+          var options = {
+            zoom: 17,
+            center: latlng,
+            mapTypeId: 'roadmap'
+          };
+          var map = new google.maps.Map(document.getElementById('map'), options);
+          var marker = new google.maps.Marker({
+              position: latlng,
+              map: map
+            });
+        }
+        // else {
+        //   alert('エラーです！');
+        // }
       }
-      // else {
-      //   alert('エラーです！');
-      // }
     }
   }
-
-}
